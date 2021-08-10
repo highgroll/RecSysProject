2c8dddd652705c0822a572a243bf0a5db847e047
highgroll <42375044+highgroll@users.noreply.github.com>
2021-08-10 21:28:38 +0300

    Курсовой проект
    Курсовой проект

--- src/metrics.py
+++ src/metrics.py
@@ -10,14 +10,6 @@ def hit_rate(recommended_list, bought_list):
     flags = np.isin(bought_list, recommended_list)
     return (flags.sum() > 0) * 1
 
-def hit_rate_at_k(recommended_list, bought_list, k=5):
-    bought_list = np.array(bought_list)
-    recommended_list = np.array(recommended_list)
-    k_recommended_list = np.array(recommended_list[:k])
-    flags = np.isin(bought_list, k_recommended_list)
-    hit_rate_at_k = (flags.sum() > 0) * 1
-    return hit_rate_at_k
-
 def precision(recommended_list, bought_list):
     bought_list = np.array(bought_list)
     recommended_list = np.array(recommended_list)
@@ -27,10 +19,56 @@ def precision(recommended_list, bought_list):
 def precision_at_k(recommended_list, bought_list, k=5):
     bought_list = np.array(bought_list)
     recommended_list = np.array(recommended_list)
-    bought_list = bought_list
-    recommended_list = recommended_list[:k]
+    bought_list = bought_list    
+    if k < len(recommended_list):
+        recommended_list = recommended_list[:k]
     flags = np.isin(bought_list, recommended_list)
     precision = flags.sum() / len(recommended_list)
     return precision
 
+def money_precision_at_k(recommended_list, bought_list, prices_recommended, k=5):
+    recommended_list = np.array(recommended_list)[:k]
+    prices_recommended = np.array(prices_recommended)[:k]
+    flags = np.isin(recommended_list, bought_list)
+    return np.dot(flags, prices_recommended).sum() / prices_recommended.sum()
+
+def recall(recommended_list, bought_list):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    flags = np.isin(bought_list, recommended_list)
+    return flags.sum() / len(bought_list)
+
+def recall_at_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    if k < len(recommended_list):
+        recommended_list = recommended_list[:k]
+    flags = np.isin(bought_list, recommended_list)
+    recall = flags.sum() / len(bought_list)
+    return recall
+
+
+def money_recall_at_k(recommended_list, bought_list, prices_recommended, prices_bought, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)[:k]
+    prices_recommended = np.array(prices_recommended)[:k]
+    prices_bought = np.array(prices_bought)
+    flags = np.isin(recommended_list, bought_list)
+    return np.dot(flags, prices_recommended).sum() / prices_bought.sum()
+
+
+def ap_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    recommended_list = recommended_list[recommended_list <= k]
+
+    relevant_indexes = np.nonzero(np.isin(recommended_list, bought_list))[0]
+    if len(relevant_indexes) == 0:
+        return 0
+    amount_relevant = len(relevant_indexes)
+
+
+    sum_ = sum(
+        [precision_at_k(recommended_list, bought_list, k=index_relevant + 1) for index_relevant in relevant_indexes])
+    return sum_ / amount_relevant
 
